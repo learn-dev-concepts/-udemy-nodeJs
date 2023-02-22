fe1cd594b02992931be1abe11afdd2d57da026a7
devsince2021 <dev.ryu2021@gmail.com>
2023-02-22 22:51:04 +0900

    create simple event emitter

---
+++ eventEmitter/sampleEmitter.js
@@ -0,0 +1,20 @@
+const Emitter = require('./emitter');
+
+const emitter = new Emitter();
+
+emitter.on('greet', function() {
+  console.log('hello world1');
+});
+
+emitter.on('greet', function() {
+  console.log("i'm back!");
+});
+
+emitter.on('bye', () => {
+  console.log('it is time to say good bye');
+});
+
+emitter.emit('bye');
+
+console.log("I'm in global");
+emitter.emit('greet');
