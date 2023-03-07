e543a93acc6623997c63c8249a051342c2c011a9
devsince2021 <dev.ryu2021@gmail.com>
2023-03-07 23:51:46 +0900

    Add clustering

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while(Date.now() - start < duration) {}
+// IS the file being executed in master mode?
+if (cluster.isMaster) {
+  // case index.js to be executed again but in child mode
+  cluster.fork();
+} else {
+  const express = require('express');
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
+
+  app.listen(3000);
 }
 
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-});
 
-app.listen(3000);
