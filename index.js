99ca754149cc9bb28a71de3e34f7b442389f628b
devsince2021 <dev.ryu2021@gmail.com>
2023-03-08 22:14:59 +0900

    Align the number of core and the number of fork

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
+
 
 // IS the file being executed in master mode?
 if (cluster.isMaster) {
   // case index.js to be executed again but in child mode
+  
   cluster.fork();
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
+  cluster.fork();  
 } else {
   const express = require('express');
+  const crypto = require('crypto');
   const app = express();
 
-  function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
-  }
 
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
+    crypto.pbkdf2('a', 'b', 500000, 512, 'sha512', () => {
+      res.send('Hi there');
+    });
+    
   });
 
   app.get('/fast', (req, res) => {
