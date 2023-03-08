9bc7776d4fa81ae74750384fc4d89ee0dc6005f3
devsince2021 <dev.ryu2021@gmail.com>
2023-03-08 21:31:38 +0900

    use cluster

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cluster = require('cluster');
 if (cluster.isMaster) {
   // case index.js to be executed again but in child mode
   cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
 } else {
   const express = require('express');
   const app = express();
@@ -16,6 +20,10 @@ if (cluster.isMaster) {
   app.get('/', (req, res) => {
     doWork(5000);
     res.send('Hi there');
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('this is fast!!');
   });
 
   app.listen(3000);
