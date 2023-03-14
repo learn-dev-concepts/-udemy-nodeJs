324a5cb9f3f9f798dd5feca20d320da7ce617d3c
devsince2021 <dev.ryu2021@gmail.com>
2023-03-15 00:37:02 +0900

    Add e2e test

---
+++ AdvancedNodeStarter-master/tests/header.test.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+let browser, page;
+
+beforeEach(async () => {
+   browser = await puppeteer.launch({ headless: false });
+   page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+test('the header has the correct text', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+  const url = await page.url();
+  expect(url).toMatch('/accounts\.google\.com/')
+});
+
+afterEach(async () => {
+  await browser.close();
+});
